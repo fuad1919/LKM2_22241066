9fc75b082cce8987d59865e2e08c7fdca8dccf45
fuad1919 <149462644+fuad1919@users.noreply.github.com>
2023-12-05 13:09:34 +0800

    Add files via upload

---
+++ classmotor.java
@@ -0,0 +1,32 @@
+package com.tugas;
+
+class motor{
+    String merk;
+    int tahun;
+    String noPolisi;
+    String warna;
+//konstruktor (menginisialisasi variabel-variabel instansi dimiliki oleh objek)
+    motor(String merk, int tahun, String noPolisi, String warna){
+        //kata kunci ths funsinya untuk merujuk pada nama yang berada dalam class motor
+        this.merk=merk;
+        this.tahun=tahun;
+        this.noPolisi=noPolisi;
+        this.warna=warna;
+    }
+//method dengan parameter tanpa return(nilai balik)
+void showInfoMotor(){
+    System.out.println("merk : "+this.merk);
+    System.out.println("tahun : "+this.tahun);
+    System.out.println("noPolisi : "+this.noPolisi);
+    System.out.println("warna : "+this.warna);
+    }
+}
+
+public class classmotor {
+//inisialisasi dengan nama MotorKU
+    public static void main(String[] args) {
+        motor motorku = new motor("honda", 1999, "dk 6 eb","ungu");
+        motorku.showInfoMotor();
+        
+    }
+}
